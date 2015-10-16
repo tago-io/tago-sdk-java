030d32bfc9e4e74e41623e3e10aef1b543da320e
rcanoff <rcanoff@gmail.com>
2015-10-15 23:55:49 -0300

    Added the method Info to check device tokens

---
+++ src/main/java/domain/DeviceInfoResult.java
@@ -0,0 +1,9 @@
+package domain;
+
+import tago.DeviceInfo;
+
+public class DeviceInfoResult {
+    public Boolean status;
+    public DeviceInfo result;
+    public String message;
+}
---
+++ src/main/java/tago/DeviceInfo.java
@@ -0,0 +1,13 @@
+package tago;
+
+public class DeviceInfo {
+
+    public String name;
+    public String description;
+    public boolean active;
+    public boolean visible;
+    public String created_at;
+    public String updated_at;
+    public String id;
+    public Bucket bucket;
+}
